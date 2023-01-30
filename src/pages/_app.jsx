9f6727ae5ae8e7c56627cef98f8bb1da7418b33a
Rizki Romadhoni <rrmdn@pm.me>
2023-01-30 14:54:07 +0700

    add social meta tags

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,6 +9,16 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="The future of digital forms" />
         <meta name="author" content="Rizki Romadhoni" />
+        <meta property="og:image" content="/images/build-printable-form.png" />
+        <meta property="og:title" content="LiteForm | Build printable forms with ease" />
+        <meta property="og:description" content="The future of digital forms" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@rrmdn__" />
+        <meta name="twitter:title" content="LiteForm | Build printable forms with ease" />
+        <meta name="twitter:description" content="The future of digital forms" />
+        <meta name="twitter:image" content="/images/build-printable-form.png" />
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
