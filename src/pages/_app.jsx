0c118d88e0304729db385bb7b108c4d722f1f575
Rizki Romadhoni <rrmdn@pm.me>
2023-01-30 16:51:52 +0700

    fix social images

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="The future of digital forms" />
         <meta name="author" content="Rizki Romadhoni" />
-        <meta property="og:image" content="/images/build-printable-form.png" />
+        <meta property="og:image" content="https://liteform.digital/images/build-printable-form.png" />
         <meta
           property="og:title"
           content="LiteForm | Build printable forms with ease"
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }) {
           name="twitter:description"
           content="The future of digital forms"
         />
-        <meta name="twitter:image" content="/images/build-printable-form.png" />
+        <meta name="twitter:image" content="https://liteform.digital/images/build-printable-form.png" />
 
         <link
           rel="apple-touch-icon"
